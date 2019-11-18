b5121c3c26a43f4ceb804cd34e815333149883d8
Sean Mitchell <40813290+seanwmitchell@users.noreply.github.com>
2019-11-18 10:11:21 +0700

    updated with comments

--- app.py
+++ app.py
@@ -9,32 +9,40 @@ app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def index():
+
+    #using request we ingest the html
     try:
         BASE_URL = 'https://tiki.vn/dien-thoai-may-tinh-bang/'
         response = requests.get(BASE_URL)
     except Exception as err:
         print(f'ERROR: {err}')
 
+    #we use the beautifulSoup library to parse the HTML
     soup = BeautifulSoup(response.text, features="html.parser")
 
-    articles = soup.find_all('div', class_='product-item')
+    #we specify the object from within the HTML
+    products = soup.find_all('div', class_='product-item')
 
+    #we create empty columns
     titles, images, prices, urls, ratings, discounts, tikinow, comments, category = [], [], [], [], [], [], [], [], []
-    for i in range(len(articles)):
-        titles.append(articles[i].find('p', class_='title').text.replace("...", "").replace(" - ", ""))
-        images.append(articles[i].img['data-src'])
-        prices.append(articles[i].find('span', class_='final-price').next)
-        urls.append(articles[i].a['href'])
-        ratings.append(int(str(articles[i].find('span', class_='rating-content').span["style"]).replace("width:", "").replace("%", ""))//20)
-        discounts.append(articles[i].find('span', class_='sale-tag sale-tag-square').next)
-        tikinow.append(len(articles[i].find_all('i', class_='tikicon icon-tikinow')) > 0)
-        comments.append(articles[i].find('p', class_='review').string)
+
+    #we parse the content and append into the columns
+    for i in range(len(products)):
+        titles.append(products[i].find('p', class_='title').text.replace("...", "").replace(" - ", ""))
+        images.append(products[i].img['data-src'])
+        prices.append(products[i].find('span', class_='final-price').next)
+        urls.append(products[i].a['href'])
+        ratings.append(int(str(products[i].find('span', class_='rating-content').span["style"]).replace("width:", "").replace("%", ""))//20)
+        discounts.append(products[i].find('span', class_='sale-tag sale-tag-square').next)
+        tikinow.append(len(products[i].find_all('i', class_='tikicon icon-tikinow')) > 0)
+        comments.append(products[i].find('p', class_='review').string)
         category.append(BASE_URL.replace("https://tiki.vn/","").replace("/","").replace("-"," ").capitalize())
 
-    final_articles = list(
+    #we combine the columns
+    final_products = list(
         zip(titles, images, prices, urls, ratings, discounts, tikinow, comments, category))
 
-    return render_template('index.html', final_articles=final_articles)
+    return render_template('index.html', final_products=final_products)
 
 if __name__ == '__main__':
     app.run(debug=True)
